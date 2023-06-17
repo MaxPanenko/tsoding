6200a2fd7e62c412bb06f98f86c9317bb2f11817
MaxPanenko <121324685+MaxPanenko@users.noreply.github.com>
2023-06-17 19:41:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -19,24 +19,43 @@ float rand_float(void)
 	return (float)rand() / (float) RAND_MAX;
 }
 
-int main()
+float cost(float w, float b)
 {
-	srand(69);
-	// y = x*w;
-	float w = rand_float() * 10.0f;
-	
 	float result = 0.0f;
 	for(size_t i = 0; i < train_count; ++i)
 	{
 		float x = train[i][0];
-		float y = x*w;
+		float y = x*w + b;
 		float d = y - train[i][1];
 		result += d*d;
-		
 	}
 	result /= train_count;
+	return result;
 
-	printf("%f\n",result);
+}
+
+int main()
+{
+	srand(time(0));
+	// y = x*w;
+	float w = rand_float() * 10.0f;
+	float b = rand_float() * 5.0f;
+
+	float eps = 1e-3;
+	float rate = 1e-3;
+
+	printf("%f\n",cost(w , b));
+	for(size_t i = 0; i < 300; ++i)
+	{
+		float dw = (cost(eps + w, b) - cost(w , b)) / eps;
+		float db = (cost(w, b + eps) - cost(w , b)) / eps;	
+		w -= rate*dw;
+		b -= rate*db;
+		printf("cost = %f, w = %f , b =%f\n", cost(w ,b) ,w,b);		
+	}
+	printf("-----------------\n");
+	printf("%f\n",w);
+	
 
 	return 0;
 }
